de5f8add8bd307e4c83427ed70f91a4e6ff0cdb3
matheusedivado <ms1501085@gmail.com>
2025-01-03 22:50:37 -0300

    Base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://matheusedivaldo.github.io/alphaacademias",
+  base: "http://alphaacademias.com.br",
 });
